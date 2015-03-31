bed2eafb01960491c3378791c89189295fa97da3
Rizwan Ahmed <rizwan.ahmed95@gmail.com>
2015-03-31 13:02:45 +0530

    This is the most stable project

---
+++ Releases/TravelAlarmv0.1_updated/app/src/main/java/com/idroidwarz/application/travelalarm/GPSMain.java
@@ -0,0 +1,80 @@
+package com.idroidwarz.application.travelalarm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.EmptyStackException;
+
+public class GPSMain extends Activity {
+
+    Button btnShowLocation,button;
+    EditText edit;
+
+    // GPSTracker class
+    GPSTracker gps;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gps);
+        edit = (EditText) findViewById(R.id.editText);
+
+        btnShowLocation = (Button) findViewById(R.id.btnShowLocation);
+
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(GPSMain.this);
+
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+
+
+
+
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+
+
+
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+}
